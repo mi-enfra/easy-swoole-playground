d8e067cee6f43e43098e7379ba82f474cbb4fb51
Marvin <marc.cr51rep@gmail.com>
2020-01-30 22:00:26 +0000

    :robot: Add cleanup script and custom Docker network

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 docker run -d --rm \
     -p 9501:9501 \
     --name easyswoole \
+    --network dev-network \
     --volume $PWD:/var/www \
     phpswoole/swoole:4.4.15-php7.1 \
     "php easyswoole start"
@@ -8,3 +9,5 @@ docker run -d --rm \
 inotify-hookable \
         --watch-directories ./App \
         --on-modify-command 'docker restart easyswoole'
+    
+trap "docker stop easyswoole" EXIT
